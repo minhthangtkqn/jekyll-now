5f7f99ff0ce723358a2870ed21fa6c3c6616a756
Thang Hoang <hoangminhthang21051995@gmail.com>
2019-05-07 18:14:15 +0700

    commit task control source code

---
+++ precache-manifest.9c0458d220eaaddac4a74400ecef815b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "56d7b1a2196afb38ff5c",
+    "url": "/static/js/main.4d4a5e9c.chunk.js"
+  },
+  {
+    "revision": "d911b512dd112c7834fb",
+    "url": "/static/js/2.9bb617e2.chunk.js"
+  },
+  {
+    "revision": "56d7b1a2196afb38ff5c",
+    "url": "/static/css/main.ff06a5cf.chunk.css"
+  },
+  {
+    "revision": "ba9ba92652841193381e7d091bffcf05",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/demo-react/precache-manifest.80a2bba8dbe5901fe52e4f06f9b6d100.js"
+  "/precache-manifest.9c0458d220eaaddac4a74400ecef815b.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demo-react/index.html"), {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
